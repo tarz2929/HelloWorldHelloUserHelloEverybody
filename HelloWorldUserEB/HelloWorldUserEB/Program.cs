6bfba4b587f40dd3dba9cc4841cd51a5488f309e
tarz2929 <saitarunmulley@gmail.com>
2020-08-08 15:22:18 -0600

    Fixed bug #001

--- HelloWorldUserEB/HelloWorldUserEB/Program.cs
+++ HelloWorldUserEB/HelloWorldUserEB/Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorldUserEB
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
         }
     }
 }

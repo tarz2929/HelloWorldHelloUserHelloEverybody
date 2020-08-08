d39391386a2b700fd7002ebe574b8ad8e31a3211
tarz2929 <saitarunmulley@gmail.com>
2020-08-08 15:55:30 -0600

    nunit test

--- HelloWorldUserEB/HelloWorldUserEB/Program.cs
+++ HelloWorldUserEB/HelloWorldUserEB/Program.cs
@@ -2,12 +2,20 @@
 
 namespace HelloWorldUserEB
 {
-    class Program
+    public class Program
     {
+        public static ConsoleKey keyPressed;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+            while (Console.ReadKey().Key != ConsoleKey.Enter)
+            {
+                Console.WriteLine("\nPress the Enter key to end this program...");
+                keyPressed = Console.ReadKey().Key;
+            }
+
+
+            }
         }
-    }
 }
